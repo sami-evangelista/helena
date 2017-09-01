e9a56e6ef67475432f3801cd19d5c7740305166f
sami-evangelista <sami.evangelista@lipn.univ-paris13.fr>
2017-09-01 20:01:59 +0200

    started the implementation of DDFS with openshmem

---
+++ src/checker/ddfs_comm.c
@@ -0,0 +1,7 @@
+#include "ddfs_comm.h"
+
+void ddfs_comm_process_explored_state
+(storage_t storage,
+ storage_id_t id) {
+  fatal_error("gros con");
+}
---
+++ src/checker/ddfs_comm.h
@@ -0,0 +1,12 @@
+#ifndef LIB_DDFS_COMM
+#define LIB_DDFS_COMM
+
+#include "includes.h"
+#include "report.h"
+#include "storage.h"
+
+void ddfs_comm_process_explored_state
+(storage_t storage,
+ storage_id_t id);
+
+#endif
