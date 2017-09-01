210518563a93ec3b6030bd1698ff7cf070148a51
sami-evangelista <sami.evangelista@lipn.univ-paris13.fr>
2017-09-01 17:09:18 +0200

    implemented hash compaction for shared memory hash table

---
+++ src/checker/hash_compaction.c
@@ -0,0 +1,24 @@
+#include "hash_compaction.h"
+
+void hash_compact
+(state_t s,
+ hash_compact_t * result) {
+  int i;
+  
+  for(i = 0; i < HASH_COMPACTION_KEYS; i++) {
+    result->keys[i] = state_hash(s);
+  }
+}
+
+bool_t hash_compact_equal
+(hash_compact_t h1,
+ hash_compact_t h2) {
+  int i;
+  
+  for(i = 0; i < HASH_COMPACTION_KEYS; i++) {
+    if(h1.keys[i] != h2.keys[i]) {
+      return FALSE;
+    }
+  }
+  return TRUE;
+}
---
+++ src/checker/hash_compaction.h
@@ -0,0 +1,18 @@
+#ifndef LIB_HASH_COMPACTION
+#define LIB_HASH_COMPACTION
+
+#include "state.h"
+
+typedef struct {
+  hash_key_t keys[HASH_COMPACTION_KEYS];
+} hash_compact_t;
+
+void hash_compact
+(state_t s,
+ hash_compact_t * result);
+
+bool_t hash_compact_equal
+(hash_compact_t h1,
+ hash_compact_t h2);
+
+#endif
